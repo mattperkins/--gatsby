207394cd29f96b8fa5ad5048a64f0a6147cbf70e
mattperkins <github@mattperkins.com>
2019-02-07 18:29:09 +0000

    cl

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,27 +2,26 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Container from '../Container'
 import avi from '../images/favicon.png'
- 
+
 const Header = () => (
-<Container>
-<h1 style={{ margin: 0 }}>
-<Link to="/">
-Link</Link></h1>
-<img src={avi} />
-</Container>
+  <Container>
+    <h1 style={{ margin: 0 }}>
+      <Link to='/'>
+        A Link</Link></h1>
+    <img src={avi} />
+  </Container>
 )
 
 const H1 = {
-backgroundColor: "pink"
+  backgroundColor: 'pink'
 }
 
 const IndexPage = () => (
-<Container>
-<Header />
-<h1 style={H1}>Projects</h1>
-<Link to="#">Link2</Link>
-</Container>
+  <Container>
+    <Header />
+    <h1 style={H1}>Projects</h1>
+    <Link to='/page2/'>Link2</Link>
+  </Container>
 )
 
 export default IndexPage
-
